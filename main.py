2acc5d52bc8fcfe3594552fc79ac9ab919af1781
Gabriel Arruda <gabrielarruda@MacBook-Air-de-Gabriel.local>
2024-01-02 22:53:54 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+st.title("Finance Dashboard")
+
+tickers = ('BTC-USD', 'ETH-USD', 'SOL-USD', 'XRP-USD', 'DOT-USD', 'ADA-USD', 'CDI', '^BVSP', '^GSPC', '^IXIC')
+
+dropdown = st.multiselect('Escolha seus ativos',
+                          tickers)
+start = st.date_input('Inicio', value = pd.to_datetime('2015-01-01'))
+end = st.date_input('Fim', value = pd.to_datetime('today'))
+
+def relativeret(df):
+    relative = df.pct_change()
+    cumret = (1+relative).cumprod() - 1
+    cumret = cumret.fillna(0)
+    return cumret
+
+if len(dropdown) > 0:
+    df = relativeret(yf.download(dropdown, start, end)['Adj Close'])
+    st.header('Retornos do {}'.format(dropdown))
+    st.line_chart(df)
+
+
